59228adabf26bc0918052e8ad50e458af39a0a95
Andrew Garratt <Andrew.GarrattN@outlook.com>
2024-04-21 14:53:09 -0700

    added styling to about page

--- src/about.js
+++ src/about.js
@@ -9,7 +9,12 @@ function loadAbout() {
     aboutHeader.innerText = 'About';
     aboutHeader.classList.add('about-header');
 
+    const aboutContent = document.createElement('p')
+    aboutContent.classList.add('about-content')
+    aboutContent.innerHTML = 'At Andrew\'s, you pay to watch me eat.  And you watch in silence.  And you will enjoy it. Dont ask how long we\'ve been in operation, because I don\'t know.'
+    
     content.appendChild(aboutHeader)
+    content.appendChild(aboutContent)
 }
 
 export { loadAbout };
--- src/menu.js
+++ src/menu.js
@@ -12,6 +12,21 @@ function loadMenu() {
         menuHeader.innerText = 'Menu'
         menuHeader.classList.add('menu-header')
         content.appendChild(menuHeader)
+        const menuItems = ['Sloppy Steaks', 'Milk Steaks']
+
+        for (let i=0; i <= 1; i++) {
+        const menuCard = document.createElement('div')
+        menuCard.classList.add('card')
+        const menuItem = document.createElement('h2')
+        menuItem.innerText = menuItems[i];
+        menuCard.appendChild(menuItem)
+        content.appendChild(menuCard)
+        }
+        
+
+        
+        
+        
     
 
 }
